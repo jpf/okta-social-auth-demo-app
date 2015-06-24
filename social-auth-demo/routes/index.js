0bdf8eeb569e84e84f29327638291cf3965ad4fd
Raymond Wang <rwang@okta.com>
2015-06-24 10:56:15 -0700

    admin page

--- social-auth-demo/routes/index.js
+++ social-auth-demo/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  var localStorage = new LocalStorage('./database');
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  var redirectUri = localStorage.getItem('redirectURI')
+  res.render('index', {
+  	'oktaFacebookLoginUrl': localStorage.getItem('facebookLoginUrl') + '?redirect_uri=' + redirectUri,
+  	'oktaGoogleLoginUrl': localStorage.getItem('googleLoginUrl') + '?redirect_uri=' + redirectUri
+  });
 });
 
 router.get('/social_auth_processing', function(req, res, next) {
